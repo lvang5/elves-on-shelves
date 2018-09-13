7728496e3d0df446cc9269ccae830abbdb03a9cc
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-13 15:00:48 -0500

    added imput fields

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Nav from '../../components/Nav/Nav';
 
 // client-side route: "/addForm"
 class AddForm extends Component {
   render() {
     return (
-      'AddForm Page'
+      <div>
+        <Nav />
+        <div>
+          <form>
+            <label>Name</label>
+            <input type="text" />
+            <br/>
+            <label>Description</label>
+            <input type="text" />
+            <br/>
+            <lable>Image Url</lable>
+            <input type="text" />
+          </form>
+        </div>
+      </div>
     );
   }
 }
