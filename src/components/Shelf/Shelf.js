2ca4af56a3164d5fee8ae86f7fbc4f79891b04a3
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-13 16:42:06 -0500

    gets shelf array on component mount

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // client-side route: "/shelf"
 class Shelf extends Component {
+  state = {
+    shelf: []
+  };
+
+  componentDidMount() {
+    axios.get('/api/shelf')
+    .then(response => {
+      this.setState({
+        shelf: response.data
+      });
+    }).catch(error => {
+      console.log(error);
+      this.props.history.push('home');
+    });
+  }
+
   render() {
     return (
-      'Shelf Page'
+      <div>{JSON.stringify(this.state.shelf)}</div>
     );
   }
 }
