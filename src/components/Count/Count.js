d102f4231ebfdc5c1f4fda911f9b5a17dd9c86aa
Sam Bahta <smabahta37@gmail.com>
2018-09-13 16:42:49 -0500

    added get route

--- src/components/Count/Count.js
+++ src/components/Count/Count.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import {connect}from 'react-redux';
+import axios from 'axios';
+
 
 // client-side route: "/count"
 class Count extends Component {
+constructor(props){
+  super(props);
+  this.state ={
+    counts:[]
+  }
+}
+componentDidMount(){
+  this.getCount();
+}
+getCount(){
+  console.log('Getting Count');
+  axios({
+    method:'GET',
+    url:'api/shelf/count'
+  }).then((response)=>{
+    console.log(response.data);
+    const action = {type:'GET_COUNT', payload: response.data};
+    this.props.dispatch(action);
+    }).catch((error)=>{
+      alert('Unable to get counts:', error);
+    })
+  }
+
   render() {
     return (
       'Count Page'
@@ -9,4 +35,5 @@ class Count extends Component {
   }
 }
 
-export default AddForm;
+
+export default connect(mapStateToProps)(Count);
