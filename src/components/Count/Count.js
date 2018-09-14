f3239028dfa6b27217a49e43beb935ceb75668c6
Sam Bahta <smabahta37@gmail.com>
2018-09-14 10:05:53 -0500

    count server route

--- src/components/Count/Count.js
+++ src/components/Count/Count.js
@@ -14,6 +14,7 @@ constructor(props){
 componentDidMount(){
   this.getCount();
 }
+
 getCount(){
   console.log('Getting Count');
   axios({
--- src/redux/reducers/shelfReducer.js
+++ src/redux/reducers/shelfReducer.js
@@ -2,4 +2,11 @@ const shelfReducer = (state = [], action) => {
   return state;
 };
 
+const counter = (state = [], action )=>{
+  switch (action.type){
+    case 'GET_COUNT':
+    return [...state,action.payload];
+  }
+  return state;
+}
 export default shelfReducer;
