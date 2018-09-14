4b171623c831fd4e46e6bcea510612f24b706d8f
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-14 10:16:04 -0500

    moved add reducer into shelfreducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,13 +2,12 @@ import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
 import shelf from './shelfReducer';
-import add from './addReducer';
+
 
 const store = combineReducers({
   user,
   login,
   shelf,
-  add,
 });
 
 export default store;
